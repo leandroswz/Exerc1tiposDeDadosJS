f7c30301ba4bbe24295d9111506f175b08be30de
Leandro Costa <leandrin.costta@gmail.com>
2022-06-09 13:23:20 -0300

    Desafio vai na web

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let nome =  "Leandro" 
+let numero = 28
+let anoAtual = 2022
+let resultado = numero - anoAtual
+console.log(resultado)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
